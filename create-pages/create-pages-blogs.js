3a32e609613e5532f1449568809acaca3c28c155
Tanay Pratap <tanaypratap@Phoenix-Soar.redmond.corp.microsoft.com>
2019-04-06 12:47:22 +0530

    extracting page creationg logic out

---
+++ create-pages/create-pages-blogs.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+function createBlogPostsPages(result, createPage) {
+
+  const blogPostTemplate = path.join(__dirname, `../src/templates/blog-post.js`);
+  const blogPosts = result.data.blogs.edges;
+  blogPosts.forEach((post, index) => {
+    const previous = index === blogPosts.length - 1 ? null : blogPosts[index + 1].node;
+    const next = index === 0 ? null : blogPosts[index - 1].node;
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
+}
+
+function graphqlForBlogs(graphql, createPage) {
+  return graphql(`
+      {
+       blogs: allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          filter: { frontmatter: { type: { eq: "blog" } } } 
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+    if (result.errors) {
+      throw result.errors;
+    }
+    // Create blogPosts pages.
+    createBlogPostsPages(result, createPage);
+  });
+}
+exports.graphqlForBlogs = graphqlForBlogs;
+
+
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,90 +1,20 @@
-const path = require(`path`)
+const { graphqlForTalks } = require("./create-pages/create-pages-talks");
+
+const { graphqlForBlogs } = require("./create-pages/create-pages-blogs");
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+
+function createIndividualPages(actions, graphql) {
+  const { createPage } = actions;
+
+  return Promise.all([
+    graphqlForBlogs(graphql, createPage),
+    graphqlForTalks(graphql, createPage)
+  ])
+}
+
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const talkPost = path.resolve('./src/templates/talk-post.js')
-  return graphql(
-    `
-      {
-        talks: allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          filter: { frontmatter: { type: { eq: "talk" } } } 
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      
-       blogs: allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          filter: { frontmatter: { type: { eq: "blog" } } } 
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    // Create blogPosts pages.
-    const blogPosts = result.data.blogs.edges
-
-    blogPosts.forEach((post, index) => {
-      const previous = index === blogPosts.length - 1 ? null : blogPosts[index + 1].node
-      const next = index === 0 ? null : blogPosts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-
-    // Create talkPosts pages.
-    const talkPosts = result.data.talks.edges
-
-    talkPosts.forEach((post, index) => {
-      const previous = index === talkPosts.length - 1 ? null : talkPosts[index + 1].node
-      const next = index === 0 ? null : talkPosts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: talkPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-  })
+  return createIndividualPages(actions, graphql);
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -99,3 +29,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
